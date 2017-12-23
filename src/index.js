41de398e52fb76eeb74fb33b473427bffd3d67ca
ink8bit <ink8bit@users.noreply.github.com>
2017-12-23 19:48:02 +0300

    Add routes, templates

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 // @flow
 
+import dotenv from 'dotenv';
 import app from './app';
 import logServerMessage from './utils';
 
-const server = app.listen(3000, () => {
+dotenv.config({ path: 'variables.env' });
+
+// Mongoose connection
+// mongoose.connect(process.env.DATABASE);
+// mongoose.Promise = global.Promise;
+// mongoose.connection.on('error', (err) => {
+//   console.log(err.message);
+// });
+
+app.set('port', process.env.PORT || 3000);
+
+const server = app.listen(app.get('port'), () => {
   logServerMessage(server);
 });
