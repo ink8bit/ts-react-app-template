ce8fcefca36ff5625cdb7bfb90e3533eec54b3eb
ink8bit <ink8bit@users.noreply.github.com>
2019-11-03 12:25:39 +0300

    Add prettier

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+/**
+ * @see {@link https://prettier.io/docs/en/configuration.html}
+ */
+module.exports = {
+  trailingComma: 'all',
+  tabWidth: 2,
+  printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  parser: 'typescript',
+};
