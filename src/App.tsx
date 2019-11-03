d520490bfc29eb24c42b452182716e8c0cb87a0c
ink8bit <ink8bit@users.noreply.github.com>
2019-11-03 14:38:42 +0300

    Add styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 
 import Header from './components/Header';
 
+import s from './global-styles.css';
+
 const App: React.FunctionComponent<{}> = () => {
   return (
-    <>
+    <div className={s.app}>
       <h1>Hello</h1>
       <Header />
-    </>
+    </div>
   );
 };
 
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-
 import { Link } from '@reach/router';
+
+import s from './styles.css';
 
 const Header: React.FunctionComponent<{}> = () => {
   return (
-    <header>
+    <header className={s.header}>
       <Link to="/">Link</Link>
     </header>
   );
