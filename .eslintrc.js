fc052164f5c8d69ac4dfd5172b457ea3cd6de137
ink8bit <ink8bit@users.noreply.github.com>
2019-11-03 12:08:36 +0300

    Add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,67 @@
+/**
+ * Configuring ESLint
+ * @see {@link https://eslint.org/docs/user-guide/configuring}
+ */
+module.exports = {
+  /**
+   * @see {@link https://eslint.org/docs/user-guide/configuring#specifying-environments}
+   */
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+
+  /**
+   * @see {@link https://eslint.org/docs/user-guide/configuring#extending-configuration-files}
+   */
+  extends: ['airbnb', 'plugin:import/errors', 'plugin:@typescript-eslint/recommended'],
+
+  /**
+   * @see {@link https://eslint.org/docs/user-guide/configuring#specifying-globals}
+   */
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+
+  /**
+   * @see {@link https://github.com/benmosher/eslint-plugin-import#importextensions}
+   */
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.ts', '.jsx', '.tsx'],
+      },
+    },
+  },
+
+  /**
+   * @see {@link https://eslint.org/docs/user-guide/configuring#specifying-parser}
+   */
+  parser: '@typescript-eslint/parser',
+
+  /**
+   * @see {@link https://eslint.org/docs/user-guide/configuring#specifying-parser-options}
+   */
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+
+  /**
+   * @see {@link https://eslint.org/docs/user-guide/configuring#configuring-plugins}
+   */
+  plugins: ['react', '@typescript-eslint', 'import'],
+
+  /**
+   * @see {@link https://eslint.org/docs/user-guide/configuring#configuring-rules}
+   */
+  rules: {
+    'arrow-parens': ['error', 'always'],
+    'no-console': 'error',
+  },
+};
