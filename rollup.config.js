656d45909dc3a43d43219c73521a22c697dde203
ink8bit <ink8bit@users.noreply.github.com>
2021-02-06 12:42:22 +0300

    Add rollup

---
+++ rollup.config.js
@@ -0,0 +1,109 @@
+import babel from '@rollup/plugin-babel';
+import typescript from '@rollup/plugin-typescript';
+import postcss from 'rollup-plugin-postcss';
+import cssnano from 'cssnano';
+import { terser } from 'rollup-plugin-terser';
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+import image from '@rollup/plugin-image';
+import html, { makeHtmlAttributes } from '@rollup/plugin-html';
+import livereload from 'rollup-plugin-livereload';
+import serve from 'rollup-plugin-serve';
+
+const isProd = process.env.NODE_ENV === 'production';
+const isDev = !isProd;
+
+function htmlTemplate({ attributes, files, publicPath, meta, title }) {
+  const scripts = (files.js || [])
+    .map(({ fileName }) => {
+      const attrs = makeHtmlAttributes(attributes.script);
+      return `<script src="${publicPath}${fileName}"${attrs}></script>`;
+    })
+    .join('\n');
+
+  const links = (files.css || [])
+    .map(({ fileName }) => {
+      const attrs = makeHtmlAttributes(attributes.link);
+      return `<link href="${publicPath}${fileName}" rel="stylesheet"${attrs}>`;
+    })
+    .join('\n');
+
+  const metas = meta
+    .map((input) => {
+      const attrs = makeHtmlAttributes(input);
+      return `<meta${attrs}>`;
+    })
+    .join('\n');
+
+  return `
+<!DOCTYPE html>
+<html ${makeHtmlAttributes(attributes.html)}>
+  <head>
+    ${metas}
+    <title>${title}</title>
+    ${links}
+  </head>
+  <body>
+    <div id="root">Rendering...</div>
+    ${scripts}
+  </body>
+</html>
+`;
+}
+
+export default {
+  input: 'src/components/App/index.tsx',
+
+  output: {
+    file: isProd ? `build/bundle_${Date.now().toString()}.js` : 'build/bundle.js',
+    format: 'iife',
+    name: 'app',
+    sourcemap: isProd ? false : 'inline',
+  },
+
+  plugins: [
+    replace({
+      'process.env.NODE_ENV': isProd ? JSON.stringify('production') : JSON.stringify('development'),
+    }),
+
+    typescript({
+      sourceMap: isDev,
+    }),
+
+    babel({ babelHelpers: 'bundled', compact: true }),
+
+    postcss({
+      plugins: [cssnano()],
+      extensions: ['.css'],
+      autoModules: false,
+      modules: {
+        generateScopedName: isProd ? '[hash:base64]' : '[path][name]_[local]_[hash:base64:5]',
+      },
+    }),
+
+    commonjs({ extensions: ['.js', '.ts', '.jsx', '.tsx'] }),
+
+    nodeResolve({ browser: true }),
+
+    html({
+      title: 'React app',
+      template: htmlTemplate,
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
+      ],
+    }),
+
+    // Run local web server to serve files when not in production
+    isDev && serve({ contentBase: 'build', port: 3000 }),
+
+    // Watch the `build` directory and refresh the
+    // browser on changes when not in production
+    isDev && livereload(),
+
+    isProd && terser(),
+
+    image(),
+  ],
+};
