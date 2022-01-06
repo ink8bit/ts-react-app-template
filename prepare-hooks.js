1e0174613c000b4ba917ae6f176c10b631b6d959
ink8bit <ink8bit@users.noreply.github.com>
2022-01-06 17:35:08 +0300

    Fix linter warnings

--- prepare-hooks.js
+++ prepare-hooks.js
@@ -1,6 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
 const isLocalEnv = !process.env.CI;
 
 if (isLocalEnv) {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  // eslint-disable-next-line global-require
   require('husky').install();
 }
